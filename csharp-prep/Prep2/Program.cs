f47b4bc4b2e82dc95dac5c89f3eaa863e4ca2939
BraxtonMedeiros <med20016@byui.edu>
2023-01-06 16:27:25 -0700

    Completed prep 1 and 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage? ");
+        string grade = Console.ReadLine();
+
+        int gradeNumber = int.Parse(grade);
+
+        string letter = "";
+
+        if(gradeNumber >= 90)
+        {
+            letter = "A";
+        }
+        else if(gradeNumber >= 80 && gradeNumber < 90)
+        {
+            letter = "B";
+        }
+        else if(gradeNumber >= 70 && gradeNumber < 80)
+        {
+            letter = "C";
+        }
+        else if(gradeNumber >= 60 && gradeNumber < 70)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}");
+
+        if(gradeNumber >= 70)
+        {
+            Console.Write("Congradulations! You passed!");
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time!");
+        }
     }
 }
